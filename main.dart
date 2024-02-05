4f966ba2d006fd23f8b4e596ea0729a9d500d538
P1MMADA <72380964+noeybin@users.noreply.github.com>
2024-02-05 21:56:56 +0700

    Add files via upload
    W4_Exercise 2: Widget from One Page from your Figma

--- main.dart
+++ main.dart
@@ -1,106 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    const String appTitle = 'Food Ingredients';
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-        title: 'Product layout demo home page',
-        key: null,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitleSection(),
+            TextSection(),
+            ImageSection(),
+            const Center(),
+          ],
+        ),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
+class TitleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Product Listing")),
-        body: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-          children: <Widget>[
-            ProductBox(
-                name: "iPhone",
-                description: "iPhone is the stylist phone ever",
-                price: 1000,
-                image: "iphone.jpg"),
-            ProductBox(
-                name: "Pixel",
-                description: "Pixel is the most featureful phone ever",
-                price: 800,
-                image: "pixel.jpg"),
-            ProductBox(
-                name: "Laptop",
-                description: "Laptop is most productive development tool",
-                price: 2000,
-                image: "laptop.jpg"),
-            ProductBox(
-                name: "Tablet",
-                description:
-                    "Tablet is the most useful device ever for meeting",
-                price: 1500,
-                image: "tablet.jpg"),
-            ProductBox(
-                name: "Pendrive",
-                description: "Pendrive is useful storage medium",
-                price: 100,
-                image: "pendrive.jpg"),
-            ProductBox(
-                name: "Floppy Drive",
-                description: "Floppy drive is useful rescue storage medium",
-                price: 20,
-                image: "floppydisk.jpg"),
-          ],
-        ));
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.blue),
+          const SizedBox(width: 8.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Pad Thai',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Text('Easy Thai Traditional Food Recipe',
+                  style: TextStyle(fontSize: 16.0)),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
-class ProductBox extends StatelessWidget {
-  ProductBox(
-      {Key? key,
-      required this.name,
-      required this.description,
-      required this.price,
-      required this.image})
-      : super(key: key);
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-
+class ImageSection extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(2),
-        height: 120,
-        child: Card(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-              Image.asset("assets/" + image),
-              Expanded(
-                  child: Container(
-                      padding: EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text(this.name,
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text(this.description),
-                          Text("Price: " + this.price.toString()),
-                        ],
-                      )))
-            ])));
+      padding: const EdgeInsets.all(16.0),
+      child: Image.network(
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Phat_Thai_kung_Chang_Khien_street_stall.jpg/1200px-Phat_Thai_kung_Chang_Khien_street_stall.jpg',
+        width: 300.0,
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        '- 250g (8.8 oz or 2 cups) Chopped/cubed Chicken Breast\n - 225g (7.9 oz or 1 cup) Minced Pork(Optional)\n - 1 cup PadThai Sauce',
+        style: TextStyle(fontSize: 18.0),
+      ),
+    );
   }
 }
